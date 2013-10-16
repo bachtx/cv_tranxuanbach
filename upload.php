0ce596ef2e4818fde064135b32920ca3ee664abb
bachtx <bachtx@smartosc.com>
2013-10-16 16:51:52 +0700

    commit

---
+++ upload.php
@@ -0,0 +1,141 @@
+<html>
+<header>
+	<meta charset='utf-8'/>
+	<title>CV Of Tran Xuan Bach</title>
+	<link type="text/css" rel="stylesheet" href="style.css">
+</header>
+<body>
+	<div id='waper'>
+	<div class='main'>
+		<div class='top'>
+			<div class='top_l'>
+				<div class='title'><h1>Curriculum Vitae</h1></div>
+			</div>
+			<div class='top_r'>
+				<div>
+					<img src='Untitled.png' width='130' height='150'/>
+				</div>
+			</div>			
+		</div>
+		<div class='info'>
+				<p><span>Address: Tu Liem - Ha Noi    |    Tel: 01656241161 |     Email: txbachit@gmail.com     |     Date of birth: 28/06/1991</span></p>
+		</div>
+		<div class='tab'>
+			<h2><span>OBJECTIVE</span></h2>
+			<p><span>Store knowledge, about PHP programming. Practice and practice personal skills to become professional programmer.</span></p>
+		</div>
+		<div class='tab'>
+			<h2><span>SUMMARY</span></h2>
+			<ul>
+				<li><p>Excellent communication and interpersonal skills, with the ability to develop strong relationships with people from a wide range of cultures.</p></li>
+				<li><p>Able to learn and master new skills quickly coupled with a high level of multi tasking and organizational abilities </p></li>
+				<li><p>Able to priorities duties, multi-task and work to set timeframes and deadlines </p></li>
+				<li><p>Motivated and hard working team member, who is able to adapt quickly to new working environments </p></li>
+			</ul>
+		</div>
+		<div class='tab'>
+			<h2><span>EDUCATION HISTORY</span></h2>
+			<table>
+				<tr>
+					<td width='200' align='center' style='border-right:1px solid #ccc;'><strong>2009-2013</strong></td><td>&nbsp;&nbsp;&nbsp;</td>
+					<td>
+						<h3>MILITARY TECHNICAL ACADEMY, HANOI,VIETNAM</h3>
+						<strong>Key papers completed:</strong>
+						<ul>
+							<li><p>Data Structures and Algorithms<p></li>
+							<li><p>Relational Database</p></li>
+							<li><p>Unified Modeling Language</p></li>
+							<li><p>Programming languages: C/C++,C#, Java, PHP</p></li>
+							<li><p>Database: MySQL, SQL Servere</p></li>
+						</ul>
+						<strong>Major achieved:</strong>
+						<ul>
+							<li><p>Maintain 7.98 grades for all of papers.<p></li>
+							<li><p>Graduate in 2014</p></li>
+						</ul>
+						
+					</td>
+				
+				</tr>
+			</table>
+		</div>
+		<div class='tab'>
+			<h2><span></span>WORK EXPERIENCE</span></h2>
+			<table>
+				<tr>
+					<td width='200' align='center' style='border-right:1px solid #ccc;'><strong>Feb 2013 – Sep 2013</strong></td><td>&nbsp;&nbsp;&nbsp;</td>
+					<td>
+						<h3>New Technology and Applications Solutions IGF Corporation</h3>
+						<strong>Position: Developed website</strong>
+						<strong>Responsibilities:</strong>
+						<ul>
+							<li><p>Learned and used website programming language and technology such as PHP and MySQL<p></li>
+							<li><p>Learned and used website programming language and technology such as ASP.NET and SQL</p></li>
+							<li><p>Programming Winform with C#.</p></li>
+							<li><p>Join contes Wowzapp 2012:Worldwide Hackathon For Window 8</p></li>
+						</ul>
+						<strong>Product:</strong>
+						<ul>
+							<li><p>http://www.simsodepvn.info/<p></li>
+							<li><p>http://liksinn.com.vn/<p></li>
+							<li><p>http://thoitrangaau.com/<p></li>
+							<li><p>http://shoptuancua.com/<p></li>
+							<li><p>http://conhocgioi.com/<p></li>
+							<li><p>http://ctylethanh.com/<p></li>
+							<li><p>http://songthao.vn/<p></li>
+							<li><p>http://hangvietgiatot.com/<p></li>
+						</ul>
+						<strong>Major achievements:</strong>
+						<ul>
+							<li><p>Received positive comments from manager<p></li>
+							<li><p>Successfully demonstrated the ability to work in team-based environments and to tight deadlines.<p></li>
+						</ul>
+					</td>
+				
+				</tr>
+			</table>
+		</div>
+		<div class='tab'>
+			<h2><span>ADDITIONAL SKILLS</span></h2>
+			<table>
+				<tr>
+					<td width='200' align='center' style='border-right:1px solid #ccc;'><strong>Feb 2013 – Sep 2013</strong></td><td>&nbsp;&nbsp;&nbsp;</td>
+					<td>
+						<h3>Computer skill</h3>
+						<ul>
+							<li><p>Proficiency in PHP, MySQL, Java and SQL Server, etc<p></li>
+							<li><p>Good knowledge in OOP, MVC pattern.</p></li>
+							<li><p>Development skill teamwork</p></li>
+							<li><p>Experienced at setting up PC desktop hardware, computers and small networks</p></li>
+							<li></p>Advanced knowledge of Microsoft Windows operating systems, including Windows Server family (Windows server 2003 and 2008) and Windows client family such as Windows XP, Windows Vista, Windows 7 and Microsoft Office applications (Word, Excel, PowerPoint, etc.).</p></li>
+						</ul>
+						<strong>Language skill:</strong>
+						<ul>
+							<li><p>Fluent (written and oral) in English and Vietnamese<p></li>	
+						</ul>
+					</td>
+				
+				</tr>
+			</table>
+		</div>
+		<div class='tab'>
+			<h2><span>REFERENCES</span></h2>
+			<table>
+				<tr>
+					<td width='200' align='center' style='border-right:1px solid #ccc;'><strong>Companny</strong></td><td>&nbsp;&nbsp;&nbsp;</td>
+					<td><strong>Nguyen Xuan Tuyen</strong>
+						<ul>
+							<li><p>Director: IGF Corporation</p></li>
+							<li><p>Phone:  0936831277</p></li>
+							<li><p>Email: nxtuyen.pro@gmail.com</p></li>
+							<li><p>Relationship: Work </p></li>
+						</ul>
+					</td>
+				
+				</tr>
+			</table>
+		</div>
+		</div>
+	</div>
+</body>
+</html>
